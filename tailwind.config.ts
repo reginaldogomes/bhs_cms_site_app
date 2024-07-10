72caa3b9b16c667e44407e70aaa717b28cd9eda0
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-07-09 22:59:19 -0300

    slide menu tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        slideUp: {
+          '0%': { transform: 'translateY(100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        slideUp: 'slideUp 0.5s ease-out',
+      },
       backgroundImage: {
         'hero-pattern': "url('/bg.jpg')",
       },
