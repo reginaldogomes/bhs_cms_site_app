91345529c8f763d2f98da142b5dadaf85a78e117
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-07-11 11:23:58 -0300

    padding body modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({ children }: LayoutProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{process.env.TITLE_SITE}</title>
       </head>
-      <body className="bg-primary-500 text-primary-100 font-nunito antialiased">
+      <body className="bg-primary-500 text-primary-100 font-nunito antialiased p-2">
         <Header />
         <main>{children}</main>
         <Footer />
