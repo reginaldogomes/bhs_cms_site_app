d1295c2194dcbb32d52f9ac080e617f32e735d18
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-08-06 23:32:00 -0300

    fix: format code

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,4 +1,4 @@
-import Container from "@/components/layouts/Container";
+import Container from "@/components/layouts/Container"
 
 export default function page() {
   return <Container>Articles</Container>
--- src/components/layouts/Container.tsx
+++ src/components/layouts/Container.tsx
@@ -4,13 +4,8 @@ interface ContainerProps {
   variant?: "fullwidth" | "1280"
 }
 
-const Container: React.FC<ContainerProps> = ({
-  children,}) => {
-  return (
-    <div className="mx-auto max-w-7xl">
-      {children}
-    </div>
-  )
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return <div className="mx-auto max-w-7xl">{children}</div>
 }
 
 export default Container
